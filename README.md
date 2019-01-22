aa5d47e6d75d5c48bdb0c65eae0f543c3dbc2806
langlangbody <m15934806918_2@163.com>
2019-01-22 19:23:11 +0800

    测试版

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
+# @xielanglang/wxll
 
-
-[![npm version](https://img.shields.io/npm/v/bootstrap.svg)](https://github.com/langlangbody/wxll)
+[![npm version](https://img.shields.io/npm/v/@xielanglang/wxll.svg)](https://github.com/langlangbody/wxll)
 
 [![npm bundle size (minified)](https://img.shields.io/npm/v/:scope/:packageName.svg)](https://github.com/langlangbody/wxll)
---
+++ Template/index.vue
@@ -0,0 +1,24 @@
+ /**
+ * @author xll
+ * @template Xteam
+ * time
+*/
+
+<template>
+  <div class="index-flash__content">
+      <index-view />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import IndexView from '@/components/index-view/container.vue'
+
+export default {
+  name: 'app-container',
+  components: {
+    'index-view': IndexView
+  }
+}
+</script>
